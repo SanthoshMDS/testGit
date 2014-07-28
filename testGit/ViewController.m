4e2df479df83510a553aa66557a29c0d969d6d8f
arunMDS <arunbhat.m@manipalsystems.com>
2014-07-28 11:35:34 +0530

    ARUN MERGE TEST

--- testGit/ViewController.m
+++ testGit/ViewController.m
@@ -20,6 +20,7 @@
 	// Do any additional setup after loading the view, typically from a nib.
     NSLog(@"hi Testing");
     NSLog(@"testbranchSanthosh");
+    NSLog(@"MERGE TEST ARUN");
 }
 
 - (void)didReceiveMemoryWarning
