a2557f1c27cc90b3bc155fcd132d1570caa7b5f2
Manju <software@Admins-Mac-mini.local>
2014-07-30 17:50:29 +0530

    //testing By manju

--- testGit/ViewController.m
+++ testGit/ViewController.m
@@ -19,6 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     NSLog(@"hi Testing");
+    NSLog(@"By manju");
 }
 
 - (void)didReceiveMemoryWarning
